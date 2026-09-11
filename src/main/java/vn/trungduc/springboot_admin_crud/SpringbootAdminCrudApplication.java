9e4c3bae402d2e0143c70eec0d28646e17e799e5
Huynh Cao Trung Duc <huynhcaotrungduc@gmail.com>
2026-09-11 22:57:07 +0700

    chore: initialize spring boot project with jsp, sql server, and layered structure

---
+++ src/main/java/vn/trungduc/springboot_admin_crud/SpringbootAdminCrudApplication.java
@@ -0,0 +1,13 @@
+package vn.trungduc.springboot_admin_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/trungduc/springboot_admin_crud/SpringbootAdminCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.trungduc.springboot_admin_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
